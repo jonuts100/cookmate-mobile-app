bf3e021d643cbfd62b77583e811a4911ff365c5a
Jonathan The <jonathanthesuyanto@gmail.com>
2025-05-17 13:22:11 +0700

    Finished with user preferences setting in create-recipe screen

--- app/screens/create-recipe.jsx
+++ app/screens/create-recipe.jsx
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, View, Alert, TouchableOpacity, Image, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, Alert, TouchableOpacity, Image, ScrollView, Pressable, FlatList } from 'react-native'
 import React, { useState } from 'react'
 import * as ImagePicker from "expo-image-picker"
 import { MaterialIcons, Ionicons } from '@expo/vector-icons'
 import 'react-native-url-polyfill/auto'
 import { generateImageCaption } from "../services/gemini"
+import { cuisineOptions , dishTypeOptions, intoleranceOptions, dietOptions } from "../../constants/data"
 import { useRouter } from "expo-router"
 import IngredientCard from '../components/ingredients-card'
 import AddIngredientModal from '../components/create-recipe/AddIngredientModal'
+import Slider from '@react-native-community/slider';
 
 const CreateRecipeScreen = () => {
     const router = useRouter()
@@ -15,6 +17,30 @@ const CreateRecipeScreen = () => {
     const [error, setError] = useState(null)
     const [ingredients, setIngredients] = useState([])
     const [modalVisible, setModalVisible] = useState(false)
+
+    // intolerances
+    const [intolerances, setIntolerances] = useState([])
+    // cuisines
+    const [cuisines, setCuisines] = useState([])
+    // diets
+    const [diets, setDiets] = useState([])
+    // dish type
+    const [dishType, setDishType] = useState([])
+    // equipment
+    const [equipmentLevel, setEquipmentLevel] = useState("Basic")
+    // protein level
+    const nutrient_points = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    const [minimumProtein, setMinimumProtein] = useState(0)
+    // fat level
+    const [minimumFat, setMinimumFat] = useState(0)
+    // carbs level
+    const [minimumCarbs, setMinimumCarbs] = useState(0)
+    // servings 
+    const SERVINGS_POINTS = [1,2,3,4,5,6,7,8,9,10]
+    const [servings, setServings] = useState(1)
+    // serving time
+    const SERVINGTIME_POINTS = [15,30,45,60]
+    const [servingTime, setServingTime] = useState(30)
 
     const pickImage = async () => {
         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
@@ -107,8 +133,21 @@ const CreateRecipeScreen = () => {
         router.push("/user-preferences")
     }
 
+    const toggleItem = (item, list, setList) => {
+        if (list.includes(item)) {
+            setList(list.filter(i => i !== item));
+        } else {
+            setList([...list, item]);
+        }
+    };
+
+    const toggleEquipmentLevel = (level) => {
+        setEquipmentLevel(level);
+    }
+    
+
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <View style={styles.headerContainer}>
                 <Text style={styles.header}>What are we cooking with?</Text>
                 <Text style={styles.subHeader}>Upload a photo of your ingredients</Text>
@@ -157,8 +196,8 @@ const CreateRecipeScreen = () => {
             </TouchableOpacity>
 
             <View style={styles.ingredientsContainer}>
-                <View style={styles.ingredientsHeader}>
-                    <Text style={styles.ingredientsTitle}>
+                <View style={styles.sectionHeader}>
+                    <Text style={styles.sectionTitle}>
                         Ingredients {ingredients.length > 0 ? `(${ingredients.length})` : ''}
                     </Text>
                     <TouchableOpacity 
@@ -189,6 +228,350 @@ const CreateRecipeScreen = () => {
                 )}
             </View>
 
+            {/* User Preferences Section */}
+            <View style={styles.userSectionContainer}>
+                <View style={styles.sectionHeader}>
+                    <Text style={styles.sectionTitle}>
+                        Cuisine {cuisines.length > 0 ? `(${cuisines.length})` : ''}
+                    </Text>
+                </View>
+                <View style={styles.optionContainer}>
+                {cuisineOptions.map((cuisine, index) => {
+                    const isSelected = cuisines.includes(cuisine);
+
+                    return (
+                        <Pressable
+                            key={index}
+                            style={[
+                                styles.optionButton,
+                                isSelected && styles.optionButtonSelected, // Apply selected style if chosen
+                            ]}
+                            onPress={() => toggleItem(cuisine, cuisines, setCuisines)}
+                        >
+                            <Text
+                                style={[
+                                    styles.optionButtonText,
+                                    isSelected && styles.optionButtonTextSelected,
+                                ]}
+                            >
+                                {cuisine}
+                            </Text>
+                        </Pressable>
+                    );
+                })}
+                </View>
+            </View>
+
+            <View style={styles.userSectionContainer}>
+                <View style={styles.sectionHeader}>
+                    <Text style={styles.sectionTitle}>
+                        Diet {diets.length > 0 ? `(${diets.length})` : ''}
+                    </Text>
+                </View>
+                
+                <View style={styles.optionContainer}>
+
+                {dietOptions.map((diet, index) => {
+                    const isSelected = diets.includes(diet);
+
+                    return (
+                        
+                        <Pressable
+                            key={index}
+                            style={[
+                                styles.optionButton,
+                                isSelected && styles.optionButtonSelected, // Apply selected style if chosen
+                            ]}
+                            onPress={() => toggleItem(diet, diets, setDiets)}
+                        >
+                            <Text
+                                style={[
+                                    styles.optionButtonText,
+                                    isSelected && styles.optionButtonTextSelected,
+                                ]}
+                            >
+                                {diet}
+                            </Text>
+                        </Pressable>
+                    );
+                })}
+                </View>
+            </View>
+
+            <View style={styles.userSectionContainer}>
+                <View style={styles.sectionHeader}>
+                    <Text style={styles.sectionTitle}>
+                        Intolerances {intolerances.length > 0 ? `(${intolerances.length})` : ''}
+                    </Text>
+                </View>
+                <View style={styles.optionContainer}>
+                {intoleranceOptions.map((intol, index) => {
+                    const isSelected = intolerances.includes(intol);
+
+                    return (
+                        <Pressable
+                            key={index}
+                            style={[
+                                styles.optionButton,
+                                isSelected && styles.optionButtonSelected, // Apply selected style if chosen
+                            ]}
+                            onPress={() => toggleItem(intol, intolerances, setIntolerances)}
+                        >
+                            <Text
+                                style={[
+                                    styles.optionButtonText,
+                                    isSelected && styles.optionButtonTextSelected,
+                                ]}
+                            >
+                                {intol}
+                            </Text>
+                        </Pressable>
+                    );
+                })}
+                </View>
+            </View>
+            
+            <View style={styles.userSectionContainer}>
+                <View style={styles.sectionHeader}>
+                    <Text style={styles.sectionTitle}>
+                        Dish Type {dishType.length > 0 ? `(${dishType.length})` : ''}
+                    </Text>
+                </View>
+                <View style={styles.optionContainer}>
+                {dishTypeOptions.map((dt, index) => {
+                    const isSelected = dishType.includes(dt);
+
+                    return (
+                        <Pressable
+                            key={index}
+                            style={[
+                                styles.optionButton,
+                                isSelected && styles.optionButtonSelected, // Apply selected style if chosen
+                            ]}
+                            onPress={() => toggleItem(dt, dishType, setDishType)}
+                        >
+                            <Text
+                                style={[
+                                    styles.optionButtonText,
+                                    isSelected && styles.optionButtonTextSelected,
+                                ]}
+                            >
+                                {dt}
+                            </Text>
+                        </Pressable>
+                    );
+                })}
+                </View>
+            </View>
+
+            <View style={styles.userSectionContainer}>
+                <View style={styles.sectionHeader}>
+                    <Text style={styles.sectionTitle}>
+                        Equipment Level
+                    </Text>
+                </View>
+                <View style={styles.optionContainer}>
+                    {["Basic", "Intermediate", "Advanced"].map((level, index) => {
+                        const isSelected = level === equipmentLevel;
+                        return (
+
+                        
+                        <Pressable
+                            key={index}
+                            style={[
+                                styles.optionButton,
+                                isSelected && styles.optionButtonSelected, // Apply selected style if chosen
+                            ]}
+                            onPress={() => toggleEquipmentLevel(level)}
+                        >
+                            <Text
+                                style={[
+                                    styles.optionButtonText,
+                                    isSelected && styles.optionButtonTextSelected,
+                                ]}
+                            >
+                                {level}
+                            </Text>
+                        </Pressable>
+                        )
+                    })}
+                    
+                </View>
+            </View>
+
+            {/* Sliders */}
+             <View style={styles.userSectionContainer}>
+                <View style={styles.sectionHeader}>
+                    <Text style={styles.sectionTitle}>
+                        Nutrients
+                    </Text>
+                </View>
+
+                <View style={styles.sliderContainer}>
+                    <Text>Minimum Carbohydrates</Text>
+                    <Slider
+                        style={{ width: "100%", height: 40 }}
+                        minimumValue={0}
+                        maximumValue={100}
+                        step={10} // Snap to checkpoints
+                        value={minimumCarbs}
+                        onValueChange={setMinimumCarbs}
+                        minimumTrackTintColor="#1fb28a"
+                        maximumTrackTintColor="#d3d3d3"
+                        thumbTintColor="#1a9274"
+                    />
+
+                </View>
+
+                <View style={styles.sliderContainer}>
+                    <Text>Minimum Protein</Text>
+                    <Slider
+                        style={{ width: "100%", height: 40 }}
+                        minimumValue={0}
+                        maximumValue={100}
+                        step={10} // Snap to checkpoints
+                        value={minimumProtein}
+                        onValueChange={setMinimumProtein}
+                        minimumTrackTintColor="#1fb28a"
+                        maximumTrackTintColor="#d3d3d3"
+                        thumbTintColor="#1a9274"
+                    />
+
+                </View>
+
+                <View style={styles.sliderContainer}>
+                    <Text>Minimum Fats</Text>
+                    <Slider
+                        style={{ width: "100%", height: 40 }}
+                        minimumValue={0}
+                        maximumValue={100}
+                        step={10} // Snap to checkpoints
+                        value={minimumFat}
+                        onValueChange={setMinimumFat}
+                        minimumTrackTintColor="#1fb28a"
+                        maximumTrackTintColor="#d3d3d3"
+                        thumbTintColor="#1a9274"
+                    />
+
+                </View>
+
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginLeft: 10,
+                    width: "95%",
+                }}>
+                    {nutrient_points.map((point, index) => (
+                    <View key={index} style={{
+                        flexDirection: "column",
+                        alignItems: "center",
+                        
+                    }}>
+                        <View style={{
+                            width: 2,
+                            height: 8,
+                            borderRadius: 0,
+                            backgroundColor: '#555',
+                            marginBottom: 2,
+                        }} />
+                        <Text>{point}</Text>
+                    </View>
+                    ))}
+                </View>
+            </View>
+            
+            <View style={styles.userSectionContainer}>
+                <View style={styles.sectionHeader}>
+                    <Text style={styles.sectionTitle}>
+                        Servings & Time
+                    </Text>
+                </View>
+
+                <View style={styles.sliderContainer}>
+                    <Text>Servings</Text>
+                    <Slider
+                        style={{ width: "100%", height: 40 }}
+                        minimumValue={1}
+                        maximumValue={10}
+                        step={1} // Snap to checkpoints
+                        value={servings}
+                        onValueChange={setServings}
+                        minimumTrackTintColor="#1fb28a"
+                        maximumTrackTintColor="#d3d3d3"
+                        thumbTintColor="#1a9274"
+                    />
+
+                    <View style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginLeft: 10,
+                        width: "95%",
+                    }}>
+                        {SERVINGS_POINTS.map((point, index) => (
+                        <View key={index} style={{
+                            flexDirection: "column",
+                            alignItems: "center",
+                            
+                        }}>
+                            <View style={{
+                                width: 2,
+                                height: 8,
+                                borderRadius: 0,
+                                backgroundColor: '#555',
+                                marginBottom: 2,
+                            }} />
+                            <Text>{point}</Text>
+                        </View>
+                        ))}
+                    </View>
+
+                </View>
+
+                <View style={styles.sliderContainer}>
+                    <Text>Cooking Time</Text>
+                    <Slider
+                        style={{ width: "100%", height: 40 }}
+                        minimumValue={15}
+                        maximumValue={60}
+                        step={15} // Snap to checkpoints
+                        value={servingTime}
+                        onValueChange={setServingTime}
+                        minimumTrackTintColor="#1fb28a"
+                        maximumTrackTintColor="#d3d3d3"
+                        thumbTintColor="#1a9274"
+                    />
+
+                    <View style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginLeft: 10,
+                        width: "95%",
+                    }}>
+                        {SERVINGTIME_POINTS.map((point, index) => (
+                        <View key={index} style={{
+                            flexDirection: "column",
+                            alignItems: "center",
+                            
+                        }}>
+                            <View style={{
+                                width: 2,
+                                height: 8,
+                                borderRadius: 0,
+                                backgroundColor: '#555',
+                                marginBottom: 2,
+                            }} />
+                            <Text>{point}</Text>
+                        </View>
+                        ))}
+                    </View>
+
+                </View>
+            </View>
+            {/* Generate Recipe */}
+
             <TouchableOpacity
                 style={[
                     styles.continueButton,
@@ -206,7 +589,7 @@ const CreateRecipeScreen = () => {
                 onClose={() => setModalVisible(false)}
                 onAdd={handleAddIngredient}
             />
-        </View>
+        </ScrollView>
     )
 }
 
@@ -342,13 +725,13 @@ const styles = StyleSheet.create({
         shadowRadius: 4,
         elevation: 2,
     },
-    ingredientsHeader: {
+    sectionHeader: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         marginBottom: 16,
     },
-    ingredientsTitle: {
+    sectionTitle: {
         fontSize: 20,
         fontWeight: "700",
         color: '#333333',
@@ -364,6 +747,53 @@ const styles = StyleSheet.create({
     ingredientsList: {
         flex: 1,
     },
+
+    userSectionContainer: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 16,
+        padding: 16,
+        marginBottom: 20,
+        shadowColor: "#000000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 2,
+
+    },
+    optionContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gapHorizontal: 8,
+    },
+    optionButton: {
+        padding: 6,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        margin: 5,
+        backgroundColor: '#fff',
+    },
+
+    optionButtonSelected: {
+        backgroundColor: '#000', // Green for selected
+        borderColor: '#000',
+    },
+
+    optionButtonText: {
+        color: '#000',
+    },
+
+    optionButtonTextSelected: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    sliderContainer: {
+        display: 'flex',
+        flexDirection: "column",
+
+    },
+    
     emptyState: {
         flex: 1,
         justifyContent: 'center',
